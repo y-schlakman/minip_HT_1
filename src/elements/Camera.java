de5e426a5c57f60078f3718315797158780ad781
Yosi Schlakman <yschlakm@g.jct.ac.il>
2021-05-13 13:04:56 +0300

    added all functions to camera class, and added camera test file

--- src/elements/Camera.java
+++ src/elements/Camera.java
@@ -1,6 +1,7 @@
 package elements;
 
 import primitives.Point3D;
+import primitives.Ray;
 import primitives.Vector;
 
 import static primitives.Util.isZero;
@@ -31,13 +32,12 @@ public class Camera {
      */
     public Camera(Point3D p0, Vector vTo, Vector vUp){
         if(!isZero(vTo.dotProduct(vUp)))
-            throw new IllegalArgumentException("ERROR: 'vTo' and 'vUp' are not orthogonal");
+            throw new IllegalArgumentException("ERROR: vTo and vUp are not orthogonal");
 
         _p0 = p0;
         _vTo=vTo.normalized();
         _vUp=vUp.normalized();
-
-        _vRight = _vTo.crossProduct(_vUp);
+        _vRight = _vTo.crossProduct(_vUp).normalize();
 
     }
 
@@ -51,15 +51,41 @@ public class Camera {
      * @return this instance of 'Camera' object.
      */
     public Camera setViewPlaneSize(double width, double height){
+        if(width <= 0)
+            throw new IllegalArgumentException("width must be positive");
+        if(height <= 0)
+            throw new IllegalArgumentException("height must be positive");
+
         _width = width;
         _height = height;
+
         return this;
     }
 
+    /**
+     * setter for distance property.
+     * @param distance distance of view plane from the camera.
+     * @return this instance of 'Camera' object.
+     */
     public Camera setDistance(double distance) {
+        if(distance <= 0)
+            throw new IllegalArgumentException("distance must be positive");
+
         _distance = distance;
+
         return this;
     }
 
-
+    /**
+     * constructs a ray through a pixel
+     * @param nX width of row
+     * @param nY height of column
+     * @param j column index of pixel
+     * @param i row index of pixel
+     * @return null for the moment as per instructions
+     */
+    public Ray constructRayThroughPixel(int nX, int nY, int j, int i) {
+        //temporarily return null as per instructions
+        return null;
+    }
 }
